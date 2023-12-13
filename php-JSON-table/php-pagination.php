c65a65e534c1463181e1e0a72cf0ae94ef8e6a4f
Байбаков Кирилл Дмитриевич <bkd@GosSort.local>
2023-12-13 11:40:30 +0300

    feat: added pagination, created table

--- php-JSON-table/php-pagination.php
+++ php-JSON-table/php-pagination.php
@@ -7,82 +7,59 @@ ini_set('memory_limit', $memory_limit);
 ini_set('include_path', '.:' . $ini_path); // Add current directory and php.ini path to include_path for easier loading of php files with require or include statements (optional)
 ?>
 <?php
+
 $json = file_get_contents('patents-example.json');
-$data = json_decode($json, true);
-$varieties_per_page = 2;
-$current_page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
-$start_from = ($current_page - 1) * $varieties_per_page;
-$end_at = $start_from + $varieties_per_page;
-$total_varieties = 0; // Initialize total number of varieties to 0 (you should replace this with the actual value)
-foreach ($data['Cultures'] as $culture) {
-  foreach ($culture['Varieties'] as $variety) {
-    $total_varieties++; // Increment total number of varieties for each variety found in the data array
-  }
-}
-$current_page_data = array_slice($data, $start_from, $varieties_per_page);
-// Initialize an array to store the cultures and their paginated varieties
-$cultures = [];
-foreach ($data['Cultures'] as $i => $culture) {
-  $cultures[$i] = [
-    'id' => $culture['id'],
-    'name' => $culture['Name'],
-    'varieties' => [],
-  ];
-  foreach ($culture['Varieties'] as $j => $variety) {
-    $cultures[$i]['varieties'][] = [
-      'id' => $variety['id'],
-      'name' => $variety['Name'],
-      'code' => $variety['Code'],
-      'query' => $variety['Query'],
-      'patent' => $variety['Patent'],
-      'closed' => $variety['Closed'],
-      'reason' => $variety['Reason'],
-    ];
-  }
-}
-// Calculate the total number of pages based on the number of varieties per page and the total number of varieties (optional)
-$total_pages = ceil($total_varieties / $varieties_per_page);
+$dataArray = json_decode($json, true);
 
-function debug_to_console($data) {
-  $output = $data;
-  if (is_array($output))
-      $output = implode(',', $output);
+$culturesPerPage = 5;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$startIndex = ($page - 1) * $culturesPerPage;
+$endIndex = $startIndex + $culturesPerPage - 1;
 
-  echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
+$cultures = array_slice($dataArray['Cultures'], $startIndex, $culturesPerPage);
+
+
+
+$totalCultures = count($dataArray['Cultures']);
+$totalPages = ceil($totalCultures / $culturesPerPage);
+
+
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
-	<title>Патенты утратившие силу</title>
+    <title>Патенты утратившие силу</title>
+    <link rel="stylesheet" media="screen" href="style.php">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-		<h1>Патенты утратившие силу</h1>
-		<?php for ($i = 0; $i < count($current_page_data); $i++): ?>
-      <?php debug_to_console($current_page_data["Cultures"][$i]["Name"]) ?>
-      <div> 
-        <h3><?php echo $current_page_data["Cultures"][$i]["Name"]; ?></h3> 
-        <?php foreach ($current_page_data["Cultures"][$i]["Varieties"] as $j => $variety): ?> 
-          <p><?php echo $variety["Name"]; ?></p> 
-        <?php endforeach; ?> 
-      </div> 
+    <?php 
+    echo '<table class="table">';
+    echo '<tr class="table__row"><th class="table__header">Код</th><th class="table__header">Заявка</th><th class="table__header">Название</th><th class="table__header">Патент</th><th class="table__header">Окончание действия</th><th class="table__header">Причина</th></tr>';
+    foreach ($cultures as $culture) {
+        echo '<tr class="table__row">';
+        echo '<td class="table__cell">' . $culture['Name'] . '</td>';
+        foreach ($culture['Varieties'] as $variety) {
+          echo '<tr class="table__row">';
+            echo '<td class="table__cell">' . $variety['Code'] . '</td>';
+            echo '<td class="table__cell">' . $variety['Query'] . '</td>';
+            echo '<td class="table__cell">' . $variety['Name'] . '</td>';
+            echo '<td class="table__cell">' . $variety['Patent'] . '</td>';
+            echo '<td class="table__cell">' . $variety['Closed'] . '</td>';
+            echo '<td class="table__cell">' . $variety['Reason'] . '</td>';
+          echo '</tr>';
+      }
+        echo '</tr>';
+    }
+    echo '</table>';
     
-    <?php endfor; ?> 
-    <?php if ($current_page > 1): ?>
-			<a href="?page=<?php echo $current_page - 1; ?>">&laquo; Previous</a> |
-		<?php endif; ?> 
-		<?php if ($current_page < $total_pages): ?> 
-			<a href="?page=<?php echo $current_page + 1; ?>">Next &raquo;</a> |
-		<?php endif; ?> 
-		Pagination: 
-    <?php echo "Page " . $current_page . " of " . $total_pages; ?> | 
-    Total Objects: <?php echo $total_varieties; ?> | 
-    Objects per Page: <?php echo $varieties_per_page; ?> |  
-    PHP Version: <?php echo PHP_VERSION; ?> | 
-    Server Software: <?php echo $_SERVER['SERVER_SOFTWARE']; ?> | 
-    Script Name: <?php echo $_SERVER['SCRIPT_NAME']; ?> | 
-    Script Filename: <?php echo $_SERVER['PHP_SELF']; ?> | 
-    Script Path: <?php echo $_SERVER['PHP_SELF']; ?> | 
-    Document Root: <?php echo $_SERVER['DOCUMENT_ROOT']; ?> | 
-    </body>
-    </html>
+    echo '<div class="pagination">';
+    for ($i = 1; $i <= $totalPages; $i++) {
+        echo '<a class="pagination__link" href="?page=' . $i . '">' . $i . '</a> ';
+    }
+    echo '</div>';
+    ?>
+</body>
+</html>
